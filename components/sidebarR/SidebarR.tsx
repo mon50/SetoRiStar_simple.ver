fb56ed2cdf7e6540deb31f7805ad80e9a658fccb
sothy50 <stea50monte@gmail.com>
2024-06-24 07:55:22 +0900

    test

--- components/sidebarR/SidebarR.tsx
+++ components/sidebarR/SidebarR.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SidebarR = () => {
   return (
     <div className="h-full hidden sm:flex flex-col items-center w-0 sm:w-40 bg-secondary-light">
-      SidebarR
+      SidebarR_test
     </div>
   );
 };
